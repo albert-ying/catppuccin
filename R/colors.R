5419beec041c84ab5b988d70b1d4b3ba8a78192a
albert-ying <59846322+albert-ying@users.noreply.github.com>
2022-11-26 13:36:07 -0500

    update main pallette

---
+++ R/colors.R
@@ -0,0 +1,73 @@
+#' A list of color palettes from Catppuccin
+#' Catppuccin is a community-driven pastel theme that aims to be the middle ground between low and high contrast themes. It consists of 4
+#' soothing warm palettes with 26 eye-candy colors each, perfect for coding, designing, and much more! In addition, this repository tracks the
+#' development of the actual color palette, the project's documentation, organization-wide assets, resources and code samples for
+#' maintainers/developers.
+#' @export
+
+catppuccin_palettes <- list(
+  latte = c(
+    "#dd7878",
+    "#8839ef",
+    "#d20f39",
+    "#fe640b",
+    "#df8e1d",
+    "#40a02b",
+    "#04a5e5",
+    "#209fb5",
+    "#1e66f5",
+    "#dc8a78",
+    "#ea76cb",
+    "#e64553",
+    "#179299",
+    "#7287fd"
+  ),
+  frappe = c(
+    "#eebebe",
+    "#ca9ee6",
+    "#e78284",
+    "#ef9f76",
+    "#e5c890",
+    "#a6d189",
+    "#99d1db",
+    "#85c1dc",
+    "#8caaee",
+    "#f2d5cf",
+    "#f4b8e4",
+    "#ea999c",
+    "#81c8be",
+    "#babbf1"
+  ),
+  macchiato = c(
+    "#f0c6c6",
+    "#c6a0f6",
+    "#ed8796",
+    "#f5a97f",
+    "#eed49f",
+    "#a6da95",
+    "#91d7e3",
+    "#7dc4e4",
+    "#8aadf4",
+    "#f4dbd6",
+    "#f5bde6",
+    "#ee99a0",
+    "#8bd5ca",
+    "#b7bdf8"
+  ),
+  mocha = c(
+    "#f2cdcd",
+    "#cba6f7",
+    "#f38ba8",
+    "#fab387",
+    "#f9e2af",
+    "#a6e3a1",
+    "#89dceb",
+    "#74c7ec",
+    "#89b4fa",
+    "#f5e0dc",
+    "#f5c2e7",
+    "#eba0ac",
+    "#94e2d5",
+    "#b4befe"
+  )
+)
---
+++ R/scales.R
@@ -0,0 +1,94 @@
+#' Islamic palette with ramped colors
+#'
+#' @param palette Choose from \code{\link{catppuccin_palettes}} list.
+#' @param alpha Transparency.
+#' @param reverse If TRUE, the direction of the colors is reversed.
+#'
+#' @examples
+#' library(scales)
+#' show_col(catppuccin_pal()(10))
+#'
+#' filled.contour(volcano, color.palette = catppuccin_pal(), asp = 1)
+#' @importFrom grDevices colorRampPalette
+#' @export
+catppuccin_pal <- function(palette = "mocha", alpha = 1, reverse = FALSE) {
+  pal <- catppuccin_palettes[[palette]]
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  return(colorRampPalette(pal, alpha))
+}
+
+#' Setup color palette for ggplot2
+#'
+#' @rdname scale_color_catppuccin
+#' @param palette Choose from \code{\link{catppuccin_palettes}} list.
+#' @param reverse Logical, whether to reverse the order of the colors.
+#' @param alpha Transparency.
+#' @param discrete Whether to use a discrete color palette.
+#' @param ... Additional arguments to pass to \code{\link[ggplot2]{scale_color_gradientn}}.
+#' @inheritParams viridis::scale_color_viridis
+#' @importFrom ggplot2 scale_color_gradientn discrete_scale
+#' @importFrom viridis scale_color_viridis
+#' @examples
+#' library(ggplot2)
+#' ggplot(mtcars, aes(mpg, wt)) +
+#'   geom_point(aes(colour = factor(cyl))) +
+#'   scale_colour_catppuccin(palette = "ottoman")
+#' ggplot(mtcars, aes(mpg, wt)) +
+#'   geom_point(aes(colour = hp)) +
+#'   scale_colour_catppuccin(palette = "shiraz", discrete = FALSE)
+#' ggplot(data = mpg) +
+#'   geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
+#'   scale_colour_catppuccin(palette = "jerusalem")
+#' ggplot(diamonds) +
+#'   geom_bar(aes(x = cut, fill = clarity)) +
+#'   scale_fill_catppuccin()
+#' @export
+#'
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+scale_color_catppuccin <- function(..., palette = "mocha",
+                                   discrete = TRUE, alpha = 1, reverse = FALSE) {
+  if (discrete) {
+    discrete_scale("color", "catppuccin",
+      palette = catppuccin_pal(palette, alpha = alpha, reverse = reverse)
+    )
+  } else {
+    warning("This palette has not been optimized for linear perception. Use at your own risk.")
+    scale_color_gradientn(colours = catppuccin_pal(palette,
+      alpha = alpha,
+      reverse = reverse, ...
+    )(256))
+  }
+}
+
+#' @rdname scale_color_catppuccin
+#' @export
+scale_colour_catppuccin <- scale_color_catppuccin
+
+#' Setup fill palette for ggplot2
+#'
+#' @param palette Choose from \code{\link{catppuccin_palettes}} list.
+#'
+#' @inheritParams viridis::scale_fill_viridis
+#' @inheritParams catppuccin_pal
+#'
+#' @param discrete Whether to use a discrete color palette.
+#' @param ... Additional arguments to pass to \code{\link[ggplot2]{scale_fill_gradientn}}.
+#' @importFrom ggplot2 scale_fill_gradientn
+#'
+#' @export
+scale_fill_catppuccin <- function(..., palette = "mocha",
+                                  discrete = TRUE, alpha = 1, reverse = TRUE) {
+  if (discrete) {
+    discrete_scale("fill", "catppuccin",
+      palette = catppuccin_pal(palette, alpha = alpha, reverse = reverse)
+    )
+  } else {
+    warning("This palette has not been optimized for linear perception. Use at your own risk.")
+    scale_fill_gradientn(colors = catppuccin_pal(palette,
+      alpha = alpha,
+      reverse = reverse, ...
+    )(256))
+  }
+}
